db4603dca2bf157d0df267098368e4dfb1b89d0a
nghiemngoc2912 <nghiemngoc2912@gmail.com>
2025-03-11 15:46:27 +0700

    add fire zone

---
+++ Assets/Scripts/FireZone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FireZone : MonoBehaviour
+{
+    public float damagePerSecond = 10f; // Lượng sát thương mỗi giây
+    private bool isPlayerInside = false;
+    private Player player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.GetComponent<Player>();
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInside && player != null && GameManager.instance.isLive)
+        {
+            GameManager.instance.health -= damagePerSecond * Time.deltaTime;
+        }
+    }
+}
